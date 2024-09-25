65eeab4d1534e274d4cc1b6240098edbe91d904c
fof3096 <c0rr1f1ll@gmail.com>
2024-09-25 12:01:37 -0300

    Fix "Query Type"

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,10 +1,10 @@
-import { Box, Button, Checkbox, Container, FormControl, FormControlLabel, FormGroup, FormLabel, TextField, Typography } from "@mui/material";
+import { Box, Button, Checkbox, Container, FormControl, FormControlLabel, FormLabel, RadioGroup, TextField, Typography, Radio } from "@mui/material";
 
 export default function Form() {
   return (
     <Container sx={{ minHeight:"100vh", display:"grid", justifyContent:"center", alignContent:"center" }}>
       <Box maxWidth={600} p={5} borderRadius={3} boxShadow={3}>
-        <Typography variant="h5">Contact Us</Typography>
+        <Typography variant="h5" mb={3}>Contact Us</Typography>
         <FormControl component={"form"} display={"flex"} sx={{
           flexDirection:"row",
           flexWrap:"wrap",
@@ -15,14 +15,14 @@ export default function Form() {
           <TextField fullWidth label="Email Address *" variant="outlined" sx={{ borderRadius: 2 }}/>
           <FormControl fullWidth>
             <FormLabel component={"legend"}>Query Type *</FormLabel>
-            <FormGroup sx={{ border:1, display: "flex", flexDirection:"row", flexWrap: "nowrap", gap: 2 }}>
-              <FormControlLabel control={<Checkbox/>} label="General Enquiry" sx={{ m:0, flexBasis:"50%" ,border:1, borderRadius: 2}}/>
-              <FormControlLabel control={<Checkbox/>} label="Support Request" sx={{ m:0, flexBasis:"50%" ,border:1, borderRadius: 2}}/>
-            </FormGroup>
+            <RadioGroup sx={{ display: "flex", flexDirection:"row", flexWrap: "nowrap", gap: 2 }}>
+              <FormControlLabel value={"General Enquiry"} control={<Radio/>} label="General Enquiry" sx={{ m:0, flexBasis:"50%" ,border:1, borderColor: "#c4c4c4", borderRadius: 2}}/>
+              <FormControlLabel value={"Support Request"} control={<Radio/>} label="Support Request" sx={{ m:0, flexBasis:"50%" ,border:1, borderColor: "#c4c4c4", borderRadius: 2}}/>
+            </RadioGroup>
           </FormControl>
           <TextField fullWidth multiline label="Message *" variant="outlined" rows={4} sx={{ borderRadius: 2 }}/>
-          <FormControlLabel required control={<Checkbox/>} label="I consent to being concated by the team *" />
-          <Button fullWidth variant="contained" sx={{ borderRadius: 2 }}>Submit</Button>
+          <FormControlLabel required control={<Checkbox/>} label="I consent to being concated by the team" />
+          <Button fullWidth type="submit" variant="contained" sx={{ borderRadius: 2 }}>Submit</Button>
         </FormControl>
       </Box>
     </Container>
