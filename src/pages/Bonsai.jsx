e8cd65d2cb15e60612b2ab0c167ee58e07072416
fof3096 <c0rr1f1ll@gmail.com>
2024-10-02 12:35:49 -0300

    "BonsaiCallToAction" component

--- src/pages/Bonsai.jsx
+++ src/pages/Bonsai.jsx
@@ -1,6 +1,7 @@
 import BonsaiPlansPricing from "../components/bonsai/BonsaiPlansPricing";
 import BonsaiAddOns from "../components/bonsai/BonsaiAddOns";
 import BonsaiNavBar from "../components/bonsai/BonsaiNavBar";
+import BonsaiCallToAction from "../components/bonsai/BonsaiCallToAction";
 
 
 export default function Bonsai() {
@@ -12,6 +13,7 @@ export default function Bonsai() {
       <BonsaiNavBar/>
       <BonsaiPlansPricing/>
       <BonsaiAddOns/>
+      <BonsaiCallToAction/>
     </div>
   )
 }
