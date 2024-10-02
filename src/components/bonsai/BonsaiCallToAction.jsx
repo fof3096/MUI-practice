3cc40638d54aab2d50f1281e3f0c88c7d52ba0ca
fof3096 <c0rr1f1ll@gmail.com>
2024-10-02 15:16:47 -0300

    "BonsaiCallToAction" completed

--- src/components/bonsai/BonsaiCallToAction.jsx
+++ src/components/bonsai/BonsaiCallToAction.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import { Box, Button, Container, Typography } from '@mui/material'
 
 export default function BonsaiCallToAction() {
   return (
-    <div>BonsaiCallToAction</div>
+    <Container maxWidth={"md"} disableGutters sx={{ mt:10, display: "flex", justifyContent: "space-between", alignItems: "center", bgcolor: "rgb(242, 250, 255)", px: 7, py: 10}}>
+        <Box display={"flex"} flexDirection={"column"} gap={1}>
+            <Typography variant='h4' component={"p"}>It's <Typography variant='h4' color='primary' component={"span"}>your</Typography> business.</Typography>
+            <Typography variant='h4' component={"p"}>We're here to help it grow.</Typography>
+        </Box>
+        <Button disableElevation variant='contained' size='large' sx={{ px: 5, py: 2, fontWeight: "bold" }}>START FREE</Button>
+    </Container>
   )
 }
